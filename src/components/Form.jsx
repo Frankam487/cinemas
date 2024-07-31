6b20b1b9f0f67893048728b109ed71c47409c545
Frank kamagang <fkamgang06@gmail.com>
2024-07-31 21:20:19 +0200

    form styled

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,16 @@
 const Form = () => {
-    return ( 
+    return (
         <div className="form">
-           <div className="form-content">
-           <input type="text" placeholder="Entrer le titre de votre film"/>
-           <label htmlFor="">Rechercher</label>
-           </div>
-           <div className="btns">
-            <button><span>+</span></button>
-            <button><span>-</span></button>
-           </div>
+            <div className="form-content">
+                <input type="text" placeholder="Entrer le titre de votre film" />
+                <label htmlFor="">Rechercher</label>
+                <div className="btns">
+                    <button>Top<span>&#8593;</span></button>
+                    <button>Flop<span>&#8595;</span></button>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Form;
