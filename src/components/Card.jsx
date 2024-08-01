fe833f0049c208fdffd0232db66530a65a3bbe0c
Frank kamagang <fkamgang06@gmail.com>
2024-08-01 22:28:45 +0200

    other feature added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,6 +9,22 @@ const Card = ({ movie }) => {
         });
         return newDate;
     }
+    const genreFinder = () => {
+        let genreArray = [];
+        for (let i = 0; i < movie.genre_ids.length; i++) {
+            switch (movie.genre_ids[i]) {
+                case 28:
+                    genreArray.push("Action");
+                    break;
+                case 12:
+                    genreArray.push("Anime");
+                    break;
+                default:
+                    break;
+            }
+        }
+        return genreArray.map((genre, index) => <li key={index}>{genre}</li>)
+    }
     return (
         <div className="card">
             <img src={"https://image.tmdb.org/t/p/original" + movie.poster_path
@@ -18,6 +34,13 @@ const Card = ({ movie }) => {
                 }</h2>
                 <p>Sorti le : {dateProcess(movie.release_date)}</p>
                 <h5>{movie.popularity.toFixed(1)} <span>⭐</span></h5>
+                <ul>
+                    {
+                        movie.genre_ids ? genreFinder() : null
+                    }
+                </ul>
+                <p>{movie.overview}</p>
+                <button>Ajouter aux favories</button>
             </div>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${inputValue}&commlanguage=fr-FR`).then((res) =>
             setMovies(res.data.results));
-    }, [inputValue])
-   
+    }, [inputValue]);
+    
     return (
         <div className="home">
             <Header />
@@ -28,17 +28,17 @@ const Home = () => {
             <div className="home-container">
                 {
                     movies
-                        .slice(0,12)
                         .sort((a, b) => {
-                            if(goodToBad === "good"){
+                            if (goodToBad === "good") {
                                 return b.popularity - a.popularity
-                            } else if(goodToBad === "nOgood") {
+                            } else if (goodToBad === "nOgood") {
                                 return a.popularity - b.popularity
                             }
                         })
                         .map((movie, index) => (
                             <Card key={index} movie={movie} />
                         ))
+                        .slice(0, 12)
                 }
             </div>
         </div>
