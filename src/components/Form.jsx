6664de31babaa0a5d121f0faece46b94b0cff84c
Frank kamagang <fkamgang06@gmail.com>
2024-08-01 08:56:16 +0200

    sort method added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,9 @@
+import { useState } from "react";
+
 const Form = () => {
+   
     return (
-        <div className="form">
-            <div className="form-content">
-                <input type="text" placeholder="Entrer le titre de votre film" />
-                <label htmlFor="">Rechercher</label>
-                <div className="btns">
-                    <button>Top<span>&#8593;</span></button>
-                    <button>Flop<span>&#8595;</span></button>
-                </div>
-            </div>
-        </div>
+       <div className="app"></div>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,43 @@ import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import axios from "axios";
 import Card from "../components/Card";
-import  Form  from "../components/Form";
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [goodToBad, setBadToGood] = useState("");
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&commlanguage=fr-FR`).then((res) =>
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${inputValue}&commlanguage=fr-FR`).then((res) =>
             setMovies(res.data.results));
-        console.log(movies);
-    }, [])
+    }, [inputValue])
+   
     return (
         <div className="home">
             <Header />
-            <Form/>
+            <div className="form">
+                <div className="form-content">
+                    <input type="text" onChange={(e) => setInputValue(e.target.value)} placeholder="Entrer le titre de votre film" />
+                    <label htmlFor="">Rechercher</label>
+                    <div className="btns">
+                        <button onClick={() => setBadToGood("good")}>Top<span >&#8593;</span></button>
+                        <button onClick={() => setBadToGood("nOgood")}>Flop<span>&#8595;</span></button>
+                    </div>
+                </div>
+            </div>
             <div className="home-container">
                 {
-                    movies.map((movie, index) => (
-                        <Card key={index} movie={movie} />
-                    ))
+                    movies
+                        .slice(0,12)
+                        .sort((a, b) => {
+                            if(goodToBad === "good"){
+                                return b.popularity - a.popularity
+                            } else if(goodToBad === "nOgood") {
+                                return a.popularity - b.popularity
+                            }
+                        })
+                        .map((movie, index) => (
+                            <Card key={index} movie={movie} />
+                        ))
                 }
             </div>
         </div>
